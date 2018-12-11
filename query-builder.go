8f86e6bc7aff112b69dc6bc481628f6dbeb6a31d
Yves Peckstadt <yves-pkstd@indetail.co.jp>
2018-12-11 17:50:04 +0900

    added many features

--- query-builder.go
+++ query-builder.go
@@ -2,12 +2,11 @@ package couchdbquerybuilder
 
 import (
 	"encoding/json"
-	"fmt"
 	"errors"
+	"fmt"
+	"reflect"
+	"strings"
 )
-
-// TODO sorting
-// paging
 
 // QueryBuilder used to generate couchDB queries
 type QueryBuilder struct {
@@ -16,8 +15,44 @@ type QueryBuilder struct {
 	docType string
 	filters map[string]interface{}
 	conditions map[string]interface{}
+	combinations []*Combination
+	sort []map[string]string
 	hasSelector bool
+	hasLimit bool
+	hasSkip bool
+	limit int
+	skip int
 }
+
+// Filter used to filter on a single field
+type Filter struct {
+	Field string
+	Value interface{}
+}
+
+// Combination used for and,or,nor,all operators
+type Combination struct {
+	Type CombinationType
+	Value []interface{}
+	Filters []Filter
+	Conditions []interface{}
+	Combinations []*Combination
+	Builder *QueryBuilder
+}
+
+// CombinationType type of combination
+type CombinationType string
+
+const (
+	// Matches if all the selectors in the array match.
+	AND   CombinationType = "$and"
+	// Matches if any of the selectors in the array match. All selectors must use the same index.
+	OR   CombinationType = "$or"
+	// Matches an array value if it contains all the elements of the argument array.
+	ALL   CombinationType = "$all"
+	// Matches if none of the selectors in the array match.
+	NOR   CombinationType = "$nor"
+)
 
 // New create a new instance of the QueryBuilder
 func New() *QueryBuilder {
@@ -30,14 +65,12 @@ func New() *QueryBuilder {
 // Build constructs the query and outputs the final result
 func (builder *QueryBuilder) Build() (string,error) {
 
-
 	if !builder.hasSelector {
 		return "", errors.New("no doctype or filters have been added for the selector")
 	}
 
 	// Initial declaration
-	queryMap := map[string]interface{}{
-	}
+	queryMap := map[string]interface{}{}
 
 	// add fields
 	if len(builder.fields) > 0 {
@@ -55,7 +88,6 @@ func (builder *QueryBuilder) Build() (string,error) {
 		}
 
 		// add doc type
-		fmt.Println(builder.docType)
 		if builder.docType != "" {
 			selectorMap["docType"] = builder.docType
 		}
@@ -68,11 +100,33 @@ func (builder *QueryBuilder) Build() (string,error) {
 		}
 
 		// add conditions
-		if len(builder.filters) > 0 {
+		if len(builder.conditions) > 0 {
 			for k, v := range builder.conditions {
 				selectorMap[k] = v
 			}
 		}
+
+		// combinations
+		if len(builder.combinations) > 0 {
+			for _, combination := range builder.combinations {
+				addCombinationToRoot(selectorMap, combination)
+			}
+		}
+	}
+
+	// add sort
+	if len(builder.sort) > 0 {
+		queryMap["sort"] = builder.sort
+	}
+
+	// add limit
+	if builder.hasLimit {
+		queryMap["limit"] = builder.limit
+	}
+
+	// add skip
+	if builder.hasSkip {
+		queryMap["skip"] = builder.skip
 	}
 
 	bytes, err := json.Marshal(&queryMap)
@@ -84,26 +138,145 @@ func (builder *QueryBuilder) Build() (string,error) {
 
 
 // AddField adds a field to the couchDB query
-func (builder *QueryBuilder) AddField(fields ...string) {
+func (builder *QueryBuilder) AddField(fields ...string) *QueryBuilder {
 	builder.fields = append(builder.fields, fields...)
+	return builder
 }
 
 
 // AddFilter adds a filter to filter on in the couchDB query
-func (builder *QueryBuilder) AddFilter(field string, value interface{}) {
+func (builder *QueryBuilder) AddFilter(field string, value interface{}) *QueryBuilder {
 	builder.filters[field] = value
 	builder.hasSelector = true
+	return builder
 }
 
 // SetDocType set the main doc type for the couchDB query
-func (builder *QueryBuilder) SetDocType(docType string) {
+func (builder *QueryBuilder) SetDocType(docType string) *QueryBuilder {
 	builder.docType = docType
 	builder.hasSelector = true
+	return builder
+}
+
+// SetLimit sets the limit for paging
+func (builder *QueryBuilder) SetLimit(limit int) *QueryBuilder {
+	builder.limit = limit
+	builder.hasLimit = true
+	return builder
+}
+
+// SetSkip sets the skip value for paging
+func (builder *QueryBuilder) SetSkip(skip int) *QueryBuilder {
+	builder.skip = skip
+	builder.hasSkip = true
+	return builder
 }
 
 // AddCondition adds a pre-defined CouchDB condition filter to the CouchDB query
-func (builder *QueryBuilder) AddCondition(field string, condition interface{}) {
+func (builder *QueryBuilder) AddCondition(field string, condition interface{}) *QueryBuilder {
 	builder.conditions[field] = condition
 	builder.hasSelector = true
+	return builder
 }
 
+// AddSort adds a field to sort on in the couchDB query
+func (builder *QueryBuilder) AddSort(field string, sortOrder string) *QueryBuilder {
+	sort :=  map[string]string{}
+	sort[field] = strings.ToLower(sortOrder)
+	builder.sort = append(builder.sort, sort)
+	return builder
+}
+
+// AddCombination adds a combination to the builder query
+func (builder *QueryBuilder) AddCombination(combinationType CombinationType, filters ...interface{}) *Combination {
+	combination := Combination{Type:combinationType,Builder:builder}
+
+	for _, filter := range filters {
+		typeName := reflect.TypeOf(filter).Name()
+		if typeName == "Filter" {
+			original, ok := filter.(Filter)
+			if ok {
+				combination.Filters = append(combination.Filters, original)
+			}
+		} else {
+			combination.Conditions =  append(combination.Conditions, filter)
+		}
+	}
+
+	builder.combinations = append(builder.combinations, &combination)
+	return &combination
+}
+
+// AddCombination adds a combination to an existing one for nesting
+func (c *Combination) AddCombination(combinationType CombinationType, filters ...interface{}) *Combination {
+	combination := Combination{Type:combinationType}
+
+	// loop through filters and check type, if filter then create custom, if condition add as condition
+	for _, filter := range filters {
+		typeName := reflect.TypeOf(filter).Name()
+		if typeName == "Filter" {
+			original, ok := filter.(Filter)
+			if ok {
+				combination.Filters = append(combination.Filters, original)
+			}
+		} else {
+			combination.Conditions =  append(combination.Conditions, filter)
+		}
+	}
+
+	c.Combinations = append(c.Combinations, &combination)
+
+	return &combination
+}
+
+
+// addCombinationToRoot converts the combination to a format that is useable by CouchDB
+func addCombinationToRoot(root map[string]interface{}, combination *Combination) {
+
+	combinationType := string(combination.Type)
+	combinationRoot := map[string][]interface{}{}
+
+	//loop through filters
+	for _, filter := range combination.Filters {
+		filterMap := map[string]interface{}{}
+		filterMap[filter.Field] = filter.Value
+		combinationRoot[combinationType] = append(combinationRoot[combinationType], filterMap)
+	}
+
+	// loop through combinations
+	if len(combination.Combinations) > 0 {
+		fmt.Println("has child combinations")
+	}
+	for _, child := range combination.Combinations {
+		combinationRoot[combinationType] = addCombinationToParent(combinationRoot[combinationType] ,child)
+	}
+
+	root[combinationType] = combinationRoot[combinationType]
+}
+
+func addCombinationToParent(parent []interface{}, combination *Combination) []interface{} {
+	combinationType := string(combination.Type)
+	combinationRoot := map[string][]interface{}{}
+
+	//loop through filters
+	for _, filter := range combination.Filters {
+		filterMap := map[string]interface{}{}
+		filterMap[filter.Field] = filter.Value
+		combinationRoot[combinationType] = append(combinationRoot[combinationType], filterMap)
+	}
+
+	// loop through combinations
+	for _, child := range combination.Combinations {
+		combinationRoot[combinationType] = addCombinationToParent(combinationRoot[combinationType], child)
+	}
+	parent = append(parent, combinationRoot)
+	return parent
+}
+
+
+
+
+// TODO
+//$not	Selector	Matches if the given selector does not match.
+//$elemMatch	Selector	Matches and returns all documents that contain an array field with at least one element that matches all the specified query criteria.
+//$allMatch	Selector	Matches and returns all documents that contain an array field with all its elements matching all the specified query criteria.
--- query-builder_test.go
+++ query-builder_test.go
@@ -8,22 +8,28 @@ import (
 func TestQueryBuilder(t *testing.T) {
 	builder := New()
 
-	builder.SetDocType("kaigo-care-accident-info")
+	builder.SetDocType("items")
+	builder.SetLimit(10)
+	builder.SetSkip(20)
 
-	builder.AddField("fieldOne")
-	builder.AddField("fieldTwo")
+	builder.AddField("fieldOne").AddField("fieldTwo")
+	builder.AddField("fieldThree", "fieldFour").AddField("fieldFive")
 
-	builder.AddFilter("id", []int{1,2,3})
-	builder.AddFilter("name", "peckstadt")
-	builder.AddFilter("categories", []string{"a","b"})
-	builder.AddFilter("singleID", 1)
+	builder.AddFilter("id", []int{1,2,3}).AddFilter("name", "peckstadt").AddFilter("categories", []string{"a","b"}).AddFilter("singleID", 1)
 
-	builder.AddCondition("testFieldForCondition", GreaterThanCondition{Value:1})
-	builder.AddCondition("testFieldForCondition", GreaterThanOrEqualCondition{Value:1})
+	builder.AddCondition("testFieldForCondition", GreaterThanCondition{Value:1}).AddCondition("testFieldForCondition", GreaterThanOrEqualCondition{Value:1})
 	builder.AddCondition("testFieldForCondition", LessThanCondition{Value:1})
 	builder.AddCondition("testFieldForCondition", LessThanOrEqualCondition{Value:1})
 	builder.AddCondition("testFieldForCondition", EqualCondition{Value:"1"})
 	builder.AddCondition("testFieldForCondition", NotEqualCondition{Value:1})
+
+
+	builder.AddSort("docType", "desc").AddSort("createdAt", "desc")
+
+
+	root := builder.AddCombination(OR, Filter{Field:"id", Value:5},Filter{Field:"ids", Value:10})
+	levelOne := root.AddCombination(AND,Filter{Field:"a", Value:1},Filter{Field:"b", Value:2})
+	levelOne.AddCombination(OR, Filter{Field:"x", Value:0},Filter{Field:"x", Value:100})
 
 	query, _ := builder.Build()
 	fmt.Printf("the final query is* %s", query)
