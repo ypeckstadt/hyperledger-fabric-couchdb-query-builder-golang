0d10a9e1896f94e34921b638bf6fea517f5a9213
Yves Peckstadt <yves-pkstd@indetail.co.jp>
2019-02-01 16:34:11 +0900

    builder pattern

--- README.md
+++ README.md
@@ -3,29 +3,29 @@
 ```
 builder := New()
 
-	builder.SetDocType("items")
-	builder.SetLimit(10)
-	builder.SetSkip(20)
-
-	builder.AddField("fieldOne").AddField("fieldTwo")
-	builder.AddField("fieldThree", "fieldFour").AddField("fieldFive")
-
-	builder.AddFilter("id", []int{1,2,3}).AddFilter("name", "peckstadt").AddFilter("categories", []string{"a","b"}).AddFilter("singleID", 1)
-
-	builder.AddCondition("testFieldForCondition", GreaterThanCondition{Value:1}).AddCondition("testFieldForCondition", GreaterThanOrEqualCondition{Value:1})
-	builder.AddCondition("testFieldForCondition", LessThanCondition{Value:1})
-	builder.AddCondition("testFieldForCondition", LessThanOrEqualCondition{Value:1})
-	builder.AddCondition("testFieldForCondition", EqualCondition{Value:"1"})
-	builder.AddCondition("testFieldForCondition", NotEqualCondition{Value:1})
-
-
-	builder.AddSort("docType", "desc").AddSort("createdAt", "desc")
-
-
 	root := builder.AddCombination(OR, Filter{Field:"id", Value:5},Filter{Field:"ids", Value:10})
 	levelOne := root.AddCombination(AND,Filter{Field:"a", Value:1},Filter{Field:"b", Value:2})
 	levelOne.AddCombination(OR, Filter{Field:"x", Value:0},Filter{Field:"x", Value:100})
 
-	query, _ := builder.Build()
-	fmt.Printf("the final query is* %s", query)
+	query, err := builder.
+		SetDocType("items").
+		SetLimit(10).
+		SetSkip(20).
+		AddField("fieldOne").
+		AddField("fieldTwo").
+		AddField("fieldThree", "fieldFour").
+		AddField("fieldFive").
+		AddFilter("id", []int{1,2,3}).
+		AddFilter("name", "peckstadt").
+		AddFilter("categories", []string{"a","b"}).
+		AddFilter("singleID", 1).
+		AddCondition("testFieldForCondition", GreaterThanCondition{Value:1}).
+		AddCondition("testFieldForCondition", GreaterThanOrEqualCondition{Value:1}).
+		AddCondition("testFieldForCondition", LessThanCondition{Value:1}).
+		AddCondition("testFieldForCondition", LessThanOrEqualCondition{Value:1}).
+		AddCondition("testFieldForCondition", EqualCondition{Value:"1"}).
+		AddCondition("testFieldForCondition", NotEqualCondition{Value:1}).
+		AddSort("docType", "desc").
+		AddSort("createdAt", "desc").
+		Build()
 ```
--- query-builder_test.go
+++ query-builder_test.go
@@ -1,36 +1,41 @@
 package couchdbquerybuilder
 
 import (
-	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestQueryBuilder(t *testing.T) {
+
 	builder := New()
-
-	builder.SetDocType("items")
-	builder.SetLimit(10)
-	builder.SetSkip(20)
-
-	builder.AddField("fieldOne").AddField("fieldTwo")
-	builder.AddField("fieldThree", "fieldFour").AddField("fieldFive")
-
-	builder.AddFilter("id", []int{1,2,3}).AddFilter("name", "peckstadt").AddFilter("categories", []string{"a","b"}).AddFilter("singleID", 1)
-
-	builder.AddCondition("testFieldForCondition", GreaterThanCondition{Value:1}).AddCondition("testFieldForCondition", GreaterThanOrEqualCondition{Value:1})
-	builder.AddCondition("testFieldForCondition", LessThanCondition{Value:1})
-	builder.AddCondition("testFieldForCondition", LessThanOrEqualCondition{Value:1})
-	builder.AddCondition("testFieldForCondition", EqualCondition{Value:"1"})
-	builder.AddCondition("testFieldForCondition", NotEqualCondition{Value:1})
-
-
-	builder.AddSort("docType", "desc").AddSort("createdAt", "desc")
-
 
 	root := builder.AddCombination(OR, Filter{Field:"id", Value:5},Filter{Field:"ids", Value:10})
 	levelOne := root.AddCombination(AND,Filter{Field:"a", Value:1},Filter{Field:"b", Value:2})
 	levelOne.AddCombination(OR, Filter{Field:"x", Value:0},Filter{Field:"x", Value:100})
 
-	query, _ := builder.Build()
-	fmt.Printf("the final query is* %s", query)
+	query, err := builder.
+		SetDocType("items").
+		SetLimit(10).
+		SetSkip(20).
+		AddField("fieldOne").
+		AddField("fieldTwo").
+		AddField("fieldThree", "fieldFour").
+		AddField("fieldFive").
+		AddFilter("id", []int{1,2,3}).
+		AddFilter("name", "peckstadt").
+		AddFilter("categories", []string{"a","b"}).
+		AddFilter("singleID", 1).
+		AddCondition("testFieldForCondition", GreaterThanCondition{Value:1}).
+		AddCondition("testFieldForCondition", GreaterThanOrEqualCondition{Value:1}).
+		AddCondition("testFieldForCondition", LessThanCondition{Value:1}).
+		AddCondition("testFieldForCondition", LessThanOrEqualCondition{Value:1}).
+		AddCondition("testFieldForCondition", EqualCondition{Value:"1"}).
+		AddCondition("testFieldForCondition", NotEqualCondition{Value:1}).
+		AddSort("docType", "desc").
+		AddSort("createdAt", "desc").
+		Build()
+	expected := `{"fields":["fieldOne","fieldTwo","fieldThree","fieldFour","fieldFive"],"limit":10,"selector":{"$or":[{"id":5},{"ids":10},{"$and":[{"a":1},{"b":2},{"$or":[{"x":0},{"x":100}]}]}],"categories":["a","b"],"docType":"items","id":[1,2,3],"name":"peckstadt","singleID":1,"testFieldForCondition":{"$neq":1}},"skip":20,"sort":[{"docType":"desc"},{"createdAt":"desc"}]}`
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, query)
 }
